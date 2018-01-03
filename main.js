bb175933c9f9b9668d2be360c02079f944bb5291
MaliceGFS <MaliceGFS@users.noreply.github.com>
2018-01-03 14:34:15 -0600

    Made formatting changes and added productName to package.json

--- main.js
+++ main.js
@@ -2,11 +2,10 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const { app, BrowserWindow } = electron;
 
 let mainWindow;
 
-app.on('ready', function ()
-{
+app.on('ready', function () {
     mainWindow = new BrowserWindow();
 });
